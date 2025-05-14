2e7262af11c409e60091a07c12e2977511b6fddb
ilham0108 <ilhampambudiutomo01@gmail.com>
2025-05-14 10:53:13 +0700

    add session role menu

--- resources/views/layouts/sidebar-v2.blade.php
+++ resources/views/layouts/sidebar-v2.blade.php
@@ -25,7 +25,6 @@
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <!-- Add icons to the links using the .nav-icon class
         with font-awesome or any other icon font library -->
-
         @if (session('role') == 'admin' || session('role') == 'manager' || session('role') == 'developer')
             <li class="nav-item">
                 <a href="#" class="nav-link">
@@ -165,6 +164,14 @@
                 </a>
             </li>
         @endif
+        <li class="nav-item">
+            <a href="/logout" class="nav-link">
+                <i class="nav-icon fas fa-sign-out-alt"></i>
+                <p>
+                    Logout
+                </p>
+            </a>
+        </li>
     </ul>
 </nav>
 <!-- /.sidebar-menu -->
